4a1fe22f7b7a084945c6cf303858f3f8a0817878
TETEUGL <00001125017442sp@al.educacao.sp.gov.br>
2025-05-05 13:38:10 -0300

    aprendi cadastrar as coisas atraves de funçoes

---
+++ function.py
@@ -0,0 +1,66 @@
+"""
+def soma():
+    nun_01 = 9
+    nun_02 = 7
+
+    resultado = nun_01 + nun_02
+    return resultado
+#---------------------------------------------------------------
+def somarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 + num_02
+
+    return print(resultado)
+
+somarDoisNumeros(24, 98)
+
+
+#---------------------------------------------------------------
+
+def somarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 * num_02
+
+    return print(resultado)
+
+somarDoisNumeros(24, 98)
+
+#------------------------------------------------------------------------------
+
+def somarDoisNumeros(nota_01, nota_02):
+    num_01 = nota_01
+    num_02 = nota_02
+
+    resultado = num_01 % num_02
+
+    return print(resultado)
+
+somarDoisNumeros(24, 98)
+
+#----------------------------------------------------------------------------
+"""
+
+def cadastrar(nome, idade):
+
+    data = {
+        "nome":nome,
+        "idade": idade
+    }
+
+    return print(data)
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cadastrar(nome, idade)
+
+
+
+
+
+
+
+
